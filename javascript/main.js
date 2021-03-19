ae118fe16e4f7d5aac050606e91a9ccfb01fdac8
Kaiying Fu <kaiying.fu@gmail.com>
2021-03-19 23:04:41 +0800

    Create function for evaluating results of 5 rounds of game

--- javascript/main.js
+++ javascript/main.js
@@ -19,6 +19,29 @@ function userPlay() {
 }
 
 
+// Evaluate results after 5 rounds of game
+function game() {
+    let score = [0,0];
+    for (let i = 0; i < 5; i++) {
+        console.log(`ROUND #${i+1}`);
+
+        result = playRound(userPlay(), computerPlay());
+        // Update current user score
+        score[0] += result[0];
+        // Update current computer score
+        score[1] += result[1];
+        console.log(`Your score: ${score[0]} \n Computer score: ${score[1]}`);
+    }
+    if (score[0] === score[1]) {
+        console.log(`Looks like it's a draw overall. You both scored ${score[0]} points`)
+    } else if (score[0] > score[1]) {
+        console.log(`YOU'RE CHAMPION. You beat the computer ${score[0]}:${score[1]}`)
+    } else {
+        console.log(`YOU'VE LOST. The computer beat you ${score[1]}:${score[0]}`)
+    }
+}
+
+
 // Evaluate result of single round of game
 function playRound(userSelection, computerSelection) {
     // Results can be either of following
@@ -31,14 +54,17 @@ function playRound(userSelection, computerSelection) {
         || (userSelection === "Scissors" && computerSelection === "Paper")
     )
     if (userSelection === computerSelection) {
-        return `It's a draw! You both chose ${userSelection}`;
+        console.log(`It's a draw! You both chose ${userSelection}.`);
+        return [0,0];
     } else if (winningCombinations) {
-        return `You win! ${userSelection} beats ${computerSelection}`;
+        console.log(`You win! ${userSelection} beats ${computerSelection}`);
+        return [1,0];
     } else {
-        return `You lose! ${computerSelection} beats ${userSelection}`;
+        console.log(`You lose! ${computerSelection} beats ${userSelection}`);
+        return [0,1];
     }
 
 }
 
 // Test game
-console.log(playRound(userPlay(), computerPlay()));
+game();
