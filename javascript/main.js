841df0d505d834da9d372d63209f904686801019
Kaiying Fu <kaiying.fu@gmail.com>
2021-03-19 22:17:10 +0800

    Add function for obtaining valid play choice from User

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-let playOptions = ["Rock", "Paper", "Scissors"];
+let playOptions = ["rock", "paper", "scissors"];
 
 // Select a random play for the Computer
 function computerPlay() {
@@ -6,3 +6,15 @@ function computerPlay() {
 }
 
 console.log(computerPlay());
+
+// Get valid play choice from User
+function userPlay() {
+    // Prompt user to input option
+    let input = "";
+    while (!playOptions.includes(input)) {
+        input = (prompt("What's your play? Type either 'Rock', 'Paper', or 'Scissors': ")).toLowerCase();
+    }
+    return input;
+}
+
+console.log(userPlay());
